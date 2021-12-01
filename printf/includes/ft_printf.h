2c7fa5588e0de14db949c68892415827cfc4bd31
LORDAME <unlimit8.com@gmail.com>
2021-12-01 19:36:50 +0100

    fixing putnbr

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: orahmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 19:56:55 by orahmoun          #+#    #+#             */
-/*   Updated: 2021/11/29 21:37:25 by orahmoun         ###   ########.fr       */
+/*   Updated: 2021/12/01 19:31:23 by orahmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 int		ft_printf(const char *, ...);
-void    ft_putchar_fd(char c, int fd);
-void    ft_putnbr_fd(int n, int fd);
-void    ft_putnbr_u_fd(unsigned int n, int fd);
+int    ft_putchar_fd(char c, int fd);
+int    ft_putnbr_fd(int n, int fd);
+int    ft_putnbr_u_fd(unsigned int n, int fd);
 void    ft_putnbr_base(int nbr, char *base);
 void    ft_putnbr_base_ul(unsigned long nbr, char *base);
 void    ft_putstr_fd(char *s, int fd);
-void    ft_putchar_fd(char c, int fd);
+int    ft_putchar_fd(char c, int fd);
 size_t  ft_strlen(const char *s);
 #endif
--- printf/utils/ft_putnbr_fd.c
+++ printf/utils/ft_putnbr_fd.c
@@ -6,17 +6,48 @@
 /*   By: orahmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 14:52:29 by orahmoun          #+#    #+#             */
-/*   Updated: 2021/11/29 22:42:16 by orahmoun         ###   ########.fr       */
+/*   Updated: 2021/12/01 19:36:05 by orahmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/ft_printf.h"
 
-int	ft_putnbr_fd(int n, int fd)
+int decimal_len(int num)
 {
+	int	len;
+
+	if (num < 0)
+		len++;
+	len = 0;
+	while (num)
+	{
+		num = num / 10;
+		len++;
+	}
+	return len;
+}
+
+int decimal_len_u(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num)
+	{
+		num = num / 10;
+		len++;
+	}
+	return len;
+}
+
+int	ft_putnbr_fd(int nb, int fd)
+{
+	int n;
+   	n = nb;
+
 	if (n == -2147483648)
 	{
 		ft_putstr_fd("-2147483648", fd);
-		return ft_strlen("-2147483648", fd);
+		return ft_strlen("-2147483648");
 	}
 	else
 	{
@@ -29,12 +60,29 @@ int	ft_putnbr_fd(int n, int fd)
 			ft_putnbr_fd(n / 10, fd);
 		ft_putchar_fd((n % 10) + 48, fd);
 	}
+	return decimal_len(nb);
 }
 
-int	ft_putnbr_u_fd(unsigned int n, int fd)
+int	ft_putnbr_u_fd(unsigned int nb, int fd)
 {
-	static 
-		if (n / 10 != 0)
-			ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd((n % 10) + 48, fd);
+	unsigned int	n; 
+
+	n = nb;
+	if (n / 10 != 0)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + 48, fd);
+	return decimal_len_u(nb);
+}
+#include <stdio.h>
+int main()
+{
+	ft_putnbr_fd(100, 1);
+	write(1,"\n",1);
+	ft_putnbr_fd(-100, 1);
+	write(1,"\n",1);
+	ft_putnbr_u_fd(100, 1);
+	write(1,"\n",1);
+	ft_putnbr_u_fd(-100, 1);
+	write(1,"\n",1);
+	printf("%u", -100);
 }
