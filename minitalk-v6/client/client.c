cdf5da006644a2e733e51ba3903b4c671faeee16
LORDAME0 <unlimit8.com@gmail.com>
2021-12-18 19:59:33 +0100

    last push from home minitak

--- minitalk-v6/client/client.c
+++ minitalk-v6/client/client.c
@@ -1,5 +1,8 @@
 #include "../headers/header.h"
-
+int sending_msg;
+void handler(int sig){
+    sending_msg = 0;
+}
 
 int main(int n, char **args)
 {
@@ -7,7 +10,9 @@ int main(int n, char **args)
     char c;
     int server_pid = ft_atoi(args[1]);
     int i = 0;
+    sending_msg = 1;
 
+    signal(SIGUSR1, handler);
     while (i < (ft_strlen(args[2]) + 1))
     {
         c = args[2][i];
@@ -18,10 +23,13 @@ int main(int n, char **args)
                 kill(server_pid, SIGUSR1);
             else
                 kill(server_pid, SIGUSR2);
-            usleep(100);
+            /* usleep(100); */
         }
         x = 8;
         i++;
     }
-
+    while(sending_msg);
+    ft_putstr_fd("-- msg was sent to the server(", 1);
+    ft_putnbr_fd(server_pid, 1);
+    ft_putstr_fd(")\n", 1);
 }
--- minitalk-v6/server/server.c
+++ minitalk-v6/server/server.c
@@ -1,7 +1,7 @@
 #include "../headers/header.h"
 
 
-void handler(int sig)
+void receive_binary_handler(int sig, siginfo_t *info, void *context)
 {
     static int      index;
     static char     c[2];
@@ -27,24 +27,31 @@ void handler(int sig)
         if (c[0] == 0)
         {
             ft_putstr_fd(str, 1);
-            ft_putstr_fd("\n -- end of the msg\n", 1);
+            ft_putstr_fd("\n -- msg was sent from : ", 1);
+            ft_putnbr_fd(info->si_pid, 1);
+            ft_putchar_fd('\n', 1);
             free(str);
             str = NULL;
+            kill(info->si_pid, SIGUSR1);
         }
         index = 0;
         c[0] = 0;
     }
-    
-
 }
+
 
 int main()
 {
-    ft_putstr_fd("the server PID is : ", 1);
-    ft_putnbr_fd(getpid(), 1);
-    ft_putchar_fd('\n', 1);
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
-    while(1)
-        pause();
+	struct sigaction sa;
+	
+	ft_putstr_fd("the server pid : ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = receive_binary_handler;
+	sigaction (SIGUSR1, &sa, NULL);
+	sigaction (SIGUSR2, &sa, NULL);
+	while(1)
+		pause();
 }
